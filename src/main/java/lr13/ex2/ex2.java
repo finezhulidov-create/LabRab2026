4f7bb3967d448e127bcf471adaa76e2888eb0b1b
ZhulidovAl <fine.zhulidov@gmail.com>
2026-05-02 22:36:08 +0500

    in

--- src/main/java/lr13/ex2/ex2.java
+++ src/main/java/lr13/ex2/ex2.java
@@ -1,4 +1,35 @@
 package lr13.ex2;
 
+import java.util.*;
+
 public class ex2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<List<Integer>> matrix = new ArrayList<>();
+        matrix.add(Arrays.asList(1, 2, 3, 4));
+        matrix.add(Arrays.asList(5, 6, 7, 8));
+        matrix.add(Arrays.asList(9, 10, 11, 12));
+        matrix.add(Arrays.asList(13, 14, 15, 16));
+
+        try {
+            System.out.println("Введите номер столбца (от 1 до " + matrix.getFirst().size() + ")");
+            int colNum = sc.nextInt() - 1;
+            if (colNum < 0 || colNum >= matrix.getFirst().size()) {
+                throw new IndexOutOfBoundsException("Номер столбца меньше 0, либо выходит за пределы массива");
+
+            }
+            System.out.println("Row " + (colNum + 1) + ":");
+            for (List<Integer> list : matrix) {
+                System.out.println(list.get(colNum));
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Error: " + e);
+        } catch (InputMismatchException e){
+            System.err.println("Должны быть введены только числа!" + e);
+        }finally {
+            sc.close();
+        }
+    }
+
+
 }
--- src/main/java/lr13/ex2/ex3.java
+++ src/main/java/lr13/ex2/ex3.java
@@ -1,10 +1,33 @@
 package lr13.ex2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class ex3 {
-
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            List<Byte> arr = new ArrayList<>();
+            int sum = 0;
+            System.out.println("Enter array: ");
+            String str = sc.nextLine();
+            String[] parts = str.split("\\s+");
+            for (String b : parts) {
+                try {
+                    arr.add(Byte.parseByte(b));
+                } catch (NumberFormatException e) {
+                    throw new NumberFormatException("Недопустимое значение byte");
+                }
+            }
+            for (Byte b : arr) {
+                sum += b;
+            }
+            if (sum > 127 || sum < -128) {
+                throw new ArithmeticException("сумма выходит за границы типа");
+            }
+            System.out.println(sum);
+        } catch (InputMismatchException e) {
+            System.err.println("Введена строка вместо числа");
+        } catch (ArithmeticException | NumberFormatException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
